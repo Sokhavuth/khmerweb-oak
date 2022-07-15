ef7ea56aff7eb2dd6aac48efa4d308d183e45fae
Sokhavuth <vuthdevelop@gmail.com>
2022-07-15 09:25:46 +0700

    make it better

--- views/index.jsx
+++ views/index.jsx
@@ -1,18 +1,25 @@
 // views/index.jsx
 
 /** @jsx h */
-//import { h } from "https://deno.land/x/nano_jsx@v0.0.33/mod.ts"
-import * as ReactDOMServer from "https://esm.sh/react-dom@/server";
-import { createElement as h } from "https://esm.sh/react@17.0.2";
+import { h, renderSSR } from "https://deno.land/x/nano_jsx@v0.0.33/mod.ts"
 
 function Index(props){
   return(
-    <div>{props.message}</div>
+    <html>
+      <head>
+        <meta charset="UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <title>Oak Web Framework</title>
+      </head>
+      <body>
+        <h3>{props.message}</h3>
+      </body>
+    </html>
   )
 }
 
-const message = 'Web development with Oad framework'
-const str = ReactDOMServer.renderToString(<Index message={message} />)
-const html = `<!DOCTYPE html>${str}`;
+const message = 'Web development with Oak framework'
+const str = renderSSR(<Index message={message} />)
+const html = `<!DOCTYPE html>${str} `
 
 export default html
